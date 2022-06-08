da116d83984822d044e8d96f9d9543e582fe4073
flopinski <103384762+flopinski@users.noreply.github.com>
2022-06-08 12:39:24 +0300

    feat: add doctors, promo, quote

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
+  mediumZoom(".zoom", {
+    margin: 50,
+    background: "#eff9f9",
+  });
+
+
+
+  $(".certificates__items").slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: true,
+  });
+
+  
   
 });
